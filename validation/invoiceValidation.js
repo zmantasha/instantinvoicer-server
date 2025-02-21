616417de3537e18c5c0e163d28410e1dd413447d
Mantasha Zubair <mantasha@wordscloud.in>
2025-02-21 15:50:56 +0530

    No changes, re-pushing last commit

--- validation/invoiceValidation.js
+++ validation/invoiceValidation.js
@@ -4,24 +4,24 @@ const Joi = require("joi");
 const invoiceValidationSchema = Joi.object({
   userId: Joi.string(), 
   senderDetails: Joi.object({
-    logo: Joi.string().allow("", null),
-    name: Joi.string().max(50).trim().required("Sender Name must be at most 50 characters"),
-    address: Joi.string().max(60).trim().required("Sender Address must be at most 60 characters"),
+    logo: Joi.string().trim().allow("", null),
+    name: Joi.string().trim().required(),
+    address: Joi.string().trim().required(),
     // email: Joi.string().email(),
     // phone: Joi.string().min(10).max(15),
   }),
   
   recipientDetails: Joi.object({
     billTo: Joi.object({
-      name: Joi.string().max(50).trim().required(),
-      address: Joi.string().max(60).trim().required(),
+      name: Joi.string().trim().required(),
+      address: Joi.string().trim().required(),
       // email: Joi.string().email(),
       // phone: Joi.string().min(10).max(15),
     }),
     
     shipTo: Joi.object({
-      name: Joi.string().max(50).trim().allow("", null),
-      address: Joi.string().max(60).trim().allow("", null),
+      name: Joi.string().trim().allow("", null),
+      address: Joi.string().trim().allow("", null),
       // email: Joi.string().email().allow("", null),
       // phone: Joi.string().min(10).max(15).allow("", null),
     }).allow(null),  // `shipTo` is optional, can be null
@@ -37,28 +37,16 @@ const invoiceValidationSchema = Joi.object({
   }),
 
   items: Joi.array()
-  .items(
-    Joi.object({
-      id: Joi.string().required(),
-      data: Joi.alternatives().try(Joi.object(), Joi.array()).required(), // Allow any object or array
-         quantity: Joi.number().required().min(1),
+    .items(
+      Joi.object({
+        id: Joi.string().required(),
+        description: Joi.string(),
+        quantity: Joi.number().required().min(1),
         rate: Joi.number().required().min(0),
         amount: Joi.number().required().min(0),
-    })
-  )
-  .min(1)
-  .required()
-  // items: Joi.array()
-  //   .items(
-  //     Joi.object({
-  //       id: Joi.string().required(),
-  //       description: Joi.string(),
-  //       quantity: Joi.number().required().min(1),
-  //       rate: Joi.number().required().min(0),
-  //       amount: Joi.number().required().min(0),
-  //     })
-  //   )
-  //   .min(1) // Ensure at least one item
+      })
+    )
+    .min(1) // Ensure at least one item
     ,
 
   totals: Joi.object({
@@ -73,10 +61,9 @@ const invoiceValidationSchema = Joi.object({
     amountPaid: Joi.number().min(0).allow(null),
     balanceDue: Joi.number().min(0).required(),
   }).required(),
-  // previousAmountPaid: Joi.number().min(0).allow(null),
-  // previousBalanceDue: Joi.number().min(0).allow(null),
-  notes: Joi.string().allow("", null),
-  terms: Joi.string().allow("", null),
+
+  notes: Joi.string().trim().allow("", null),
+  terms: Joi.string().trim().allow("", null),
   status: Joi.string().allow("", null),
 });
 
