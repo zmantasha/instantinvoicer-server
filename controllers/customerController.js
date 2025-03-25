cdcdd45daafa40c6812a8359c7ec6eaeafce9da1
zmantasha <mantasha@wordscloud.in>
2025-03-25 19:52:50 +0530

    add verifyJWTAuth

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -37,10 +37,6 @@ class CustomerController {
           }
           // req.body.createdBy = req.user.userId;
            const customer= await CustomerServicesInstance.createCustomer(req.body) 
-          //  const user = await userModel.findById(req.user.userId);
-          //  if (!user) {
-          //      return res.status(404).json({ message: "User not found" });
-          //  }
    
           //  user.customer.push(customer);
           if (userId) {
@@ -60,28 +56,104 @@ class CustomerController {
     }
 
 
-    static searchByCustomerDisplayName=async(req,res)=>{
-       try {
-        const { displayName, firstName } = req.query; 
-        const query=[]
-        if(displayName){
-          query.push({displayName:{$regex:new RegExp(displayName,"i")}})
+    // static searchByCustomerDisplayName=async(req,res)=>{
+    //    try {
+    //     const { displayName, firstName } = req.query; 
+    //     let userId = req.user?.userId || null; 
+    //     const existingCustomer = await customerModel.findOne({
+    //       displayName: displayName,
+    //       ...(userId && { createdBy: userId }) // Only filter by createdBy if userId exists
+    //   });
+
+    //   if (!existingCustomer) {
+    //       return res.status(400).json({
+    //           message: "This display is already registered as a customer under your account."
+    //       });
+    //   }
+    //     const query=[]
+    //     if(displayName){
+    //       query.push({displayName:{$regex:new RegExp(displayName,"i")}})
+    //     }
+    //     if(firstName){
+    //       query.push({firstName:{$regex:new RegExp(firstName,"i")}})
+    //     }
+    //     if(query.length===0){
+    //       return res.status(400).json({message:"please provide a displayName and firstName to search"})
+    //     }
+    //     const customer= await customerModel.find({
+    //       $or:query
+    //     });
+    //     res.send(customer)
+    //    } catch (error) {
+    //     console.log(error)
+    //     res.status(500).json({error:error.message}) 
+    //    }
+    // }
+  //   static searchByCustomerDisplayName = async (req, res) => {
+  //     try {
+  //         const { displayName, firstName } = req.query;
+  //         let userId = req.user?.userId || null;
+  
+  //         if (!displayName && !firstName) {
+  //             return res.status(400).json({ message: "Please provide either displayName or firstName to search" });
+  //         }
+  
+  //         // Construct query dynamically
+  //         const query = [];
+  //         if (displayName) {
+  //             query.push({ displayName: { $regex: new RegExp(displayName, "i") } });
+  //         }
+  //         if (firstName) {
+  //             query.push({ firstName: { $regex: new RegExp(firstName, "i") } });
+  //         }
+  
+  //         // Find customers that match query and are created by the current user (if userId exists)
+  //         const searchQuery = { $or: query };
+  //         if (userId) {
+  //             searchQuery.createdBy = userId;
+  //         }
+  
+  //         const customers = await customerModel.find(searchQuery);
+  
+  //         res.status(200).json(customers);
+  //     } catch (error) {
+  //         console.error("Error searching customers:", error);
+  //         res.status(500).json({ error: error.message });
+  //     }
+  // };
+  static searchByCustomerDisplayName = async (req, res) => {
+    try {
+        const { displayName, firstName } = req.query;
+        const userId = req.user?.userId; // Get logged-in user's ID
+
+        if (!userId) {
+            return res.status(403).json({ message: "Unauthorized: User ID missing" });
         }
-        if(firstName){
-          query.push({firstName:{$regex:new RegExp(firstName,"i")}})
+
+        if (!displayName && !firstName) {
+            return res.status(400).json({ message: "Please provide either displayName or firstName to search" });
         }
-        if(query.length===0){
-          return req.status(400).json({message:"please provide a displayName and firstName to search"})
+
+        // Build query for search
+        const query = { createdBy: userId }; // Filter by the current user's customers
+        if (displayName) {
+            query.displayName = { $regex: new RegExp(displayName, "i") };
         }
-        const customer= await customerModel.find({
-          $or:query
-        });
-        res.send(customer)
-       } catch (error) {
-        console.log(error)
-        res.status(500).json({error:error.message}) 
-       }
+        if (firstName) {
+            query.firstName = { $regex: new RegExp(firstName, "i") };
+        }
+
+        // Fetch customers belonging to the user
+        const customers = await customerModel.find(query);
+
+        res.status(200).json(customers);
+    } catch (error) {
+        console.error("Error searching customers:", error);
+        res.status(500).json({ error: error.message });
     }
+};
+
+  
 
 
     static getAllCustomer = async(req,res)=>{
