7fb09426438461c0922a0f3d0581100f0b8641e4
mantasha zubair <mantasha@wordscloud.in>
2025-03-29 17:35:11 +0530

    Added admin authorization middleware to restrict access

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,7 +1,7 @@
 const verifyAdmin=async(req,res,next)=>{
     try {
         console.log(req.user)
-        const {admin}=req.user.roles
+        const admin=req.user.roles[0]
         console.log(admin)
        if(!admin) return res.status(401).json("admin is not access")
         res.user=admin
