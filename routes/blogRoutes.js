be5596173dc7d394329547c94b432d4da05e69e5
mantasha zubair <mantasha@wordscloud.in>
2025-03-29 17:32:52 +0530

    Added CRUD operations for blog route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,8 +2,12 @@ const express= require("express")
 const BlogController = require("../controllers/blogsController")
 const verifyauthJwttoken = require("../middleware/authMiddleware")
 const verifyAdmin = require("../middleware/adminMiddleware")
+const UserMiddleware = require("../middleware/userMiddleware")
 const router= express.Router()
 
-router.get("/",verifyauthJwttoken,verifyAdmin,BlogController.getAllBlogs)
-
+router.post("/create",verifyauthJwttoken, UserMiddleware.fetchUserIdInCollection,verifyAdmin,BlogController.createBlogs)
+router.get("/",verifyauthJwttoken, BlogController.getAllBlogs)
+router.get("/:id",verifyauthJwttoken, BlogController.getBlogsById)
+router.delete("/:id",verifyauthJwttoken, UserMiddleware.fetchUserIdInCollection,verifyAdmin,BlogController.deleteBlogs)
+router.put("/:id",verifyauthJwttoken, UserMiddleware.fetchUserIdInCollection,verifyAdmin,BlogController.updateBlogs)
 module.exports=router
